695b5f254f0b53f226db37b0da5ed9ef0606d3e6
jeferbc <jeffe.bernal@gmail.com>
2017-02-25 20:08:49 -0500

    sort and fix votes bug

--- src/components/SortButton.js
+++ src/components/SortButton.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {Button, ButtonGroup} from 'react-bootstrap';
+import store from '../store';
 const styles = {
   buttons:{
     display: 'inline-flex',
@@ -10,13 +11,34 @@ const styles = {
 }
 
 class SortButton extends Component{
+  constructor(){
+    super();
+    this.sortAscendent = this.sortAscendent.bind(this);
+    this.sortDescendent = this.sortDescendent.bind(this);
+    this.state = {
+      ascendent: false,
+      descendent: false
+    }
+  }
+  sortAscendent(event){
+    event.preventDefault();
+    store.dispatch({
+      type: "SORT_ASCENDENT",
+    });
+  }
+  sortDescendent(event){
+    event.preventDefault();
+    store.dispatch({
+      type: "SORT_DESCENDENT",
+    });
+  }
   render(){
     return(
       <div style={styles.buttons}>
         <h3>Orden: </h3>
         <ButtonGroup>
-        <Button>Ascendente</Button>
-        <Button>Descendente</Button>
+        <Button onClick={()=>this.sortAscendent(event)}>Ascendente</Button>
+        <Button onClick={()=>this.sortDescendent(event)}>Descendente</Button>
         </ButtonGroup>
       </div>
     )
--- src/store.js
+++ src/store.js
@@ -1,16 +1,45 @@
 import { createStore } from 'redux';
 import posts from './posts'
+
+function getIndex(value, arr, prop) {
+    for(var i = 0; i < arr.length; i++) {
+        if(arr[i][prop] === value) {
+            return i;
+        }
+    }
+    return -1; //to handle the case where the value doesn't exist
+}
 const reducer = (state, action) => {
   if (action.type === 'VOTE_UP'){
     let posts = state.posts;
-    posts[action.post.id - 1]['votes'] = posts[action.post.id-1].votes + 1;
+    let index = getIndex(action.post.id,posts,'id');
+    posts[index]['votes'] = posts[index].votes + 1;
     return{
       ...state,
       posts: posts
     }
   }else if(action.type === 'VOTE_DOWN'){
     let posts = state.posts;
-    posts[action.post.id - 1]['votes'] = posts[action.post.id-1].votes - 1;
+    let index = getIndex(action.post.id,posts,'id');
+    posts[index]['votes'] = posts[index].votes - 1;
+    return{
+      ...state,
+      posts: posts
+    }
+  }else if(action.type === 'SORT_ASCENDENT'){
+    let posts = state.posts;
+    posts.sort(function (a, b) {
+      return a.votes - b.votes;
+    });
+    return{
+      ...state,
+      posts: posts
+    }
+  }else if(action.type === 'SORT_DESCENDENT'){
+    let posts = state.posts;
+    posts.sort(function (a, b) {
+      return b.votes - a.votes;
+    });
     return{
       ...state,
       posts: posts
